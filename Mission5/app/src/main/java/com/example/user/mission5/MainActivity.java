1f51020a72416c6197caa9fa99e6d8692c29728a
unknown <profile2697@gmail.com>
2018-01-10 00:46:44 +0900

    [Mission5] done

--- Mission5/app/src/main/java/com/example/user/mission5/MainActivity.java
+++ Mission5/app/src/main/java/com/example/user/mission5/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.user.mission5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MENU_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivityForResult(intent, MENU_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MENU_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Toast.makeText(getApplicationContext(), extras.getString("selectedMenu"), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Mission5/app/src/main/java/com/example/user/mission5/MenuActivity.java
+++ Mission5/app/src/main/java/com/example/user/mission5/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.mission5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClickMenu(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("selectedMenu", ((Button)view).getText());
+
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
