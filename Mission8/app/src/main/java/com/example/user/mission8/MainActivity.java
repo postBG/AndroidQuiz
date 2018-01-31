98e2689c1ed654eb8500594e8ba86ee723187b4f
unknown <profile2697@gmail.com>
2018-01-31 14:21:50 +0900

    [Mission8] animation

--- Mission8/app/src/main/java/com/example/user/mission8/MainActivity.java
+++ Mission8/app/src/main/java/com/example/user/mission8/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.user.mission8;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isSearchBarVisible = false;
+
+    private Animation slideDownAnimation;
+    private Animation slideUpAnimation;
+
+    private Button searchButton;
+    private LinearLayout searchBar;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchBar = (LinearLayout) findViewById(R.id.search_bar);
+        webView = (WebView) findViewById(R.id.web_view);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://m.naver.com/");
+
+
+        slideDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+        slideDownAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                searchBar.setVisibility(View.VISIBLE);
+                isSearchBarVisible = true;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        slideUpAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                searchBar.setVisibility(View.GONE);
+                isSearchBarVisible = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    public void onClickSearchButton(View view) {
+        if (isSearchBarVisible) {
+            searchBar.startAnimation(slideUpAnimation);
+            searchButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_keyboard_arrow_down_black_24px, 0, 0, 0);
+        } else {
+            searchBar.startAnimation(slideDownAnimation);
+            searchButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_keyboard_arrow_up_black_24px, 0, 0, 0);
+        }
     }
 }
